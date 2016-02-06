2d5704118c36dcec5e0db1449c429a47c8fcd1bb
Bhalla <mbbhalla@amazon.com>
2016-02-06 17:47:30 +0000

    change

--- src/patterns/DecompositionRecursion.scala
+++ src/patterns/DecompositionRecursion.scala
@@ -10,7 +10,7 @@ object DecompositionRecursion {
   def main(args: Array[String]) {
     val list = List(new Person("jorge", 32), new Person("mariane", 40), new Person("Tim", 12))
     println(
-      list.foldLeft("<people")((accum: String, it: Person) => 
+      list.foldLeft("<people>")((accum: String, it: Person) => 
           accum + "<person>" + "<name>" + it.name + "</name>" + "<age>" + it.age + "</age></person>"  
       ) + "</people>"
     )
